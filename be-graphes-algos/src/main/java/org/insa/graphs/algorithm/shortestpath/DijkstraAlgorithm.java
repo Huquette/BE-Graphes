341f0b3c30ea8b7d5d5a75680c0844d4e86f2725
sachoman <sachadarthenucq@gmail.com>
2022-04-01 18:06:43 +0200

    debut djikstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,17 +1,42 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-public class DijkstraAlgorithm extends ShortestPathAlgorithm {
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
 
+import java.util.ArrayList;
+
+public class DijkstraAlgorithm extends ShortestPathAlgorithm {
+	
+	private Node origin;
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
     }
 
     @Override
     protected ShortestPathSolution doRun() {
+    	//debut init
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        ArrayList<Label> listeLabel = new ArrayList<Label>();
+        Graph graphe = this.data.getGraph();
+        Node noeudOrigine = data.getOrigin();
+        Node noeudTerminal = data.getDestination();
+        int nombreNoeuds = graphe.size();
+        for (int i=0; i<nombreNoeuds; i++) {
+            Label label = new Label(i);
+        	if (i == noeudOrigine.getId()) {
+        		label.setCost(0);
+        	}
+        	listeLabel.set(i, label);
+        }
+        //On a fini l'init
+        BinaryHeap<Label> tasLabel = new BinaryHeap<Label>();
+        tasLabel.insert(listeLabel.get(0));
+        while((!tasLabel.isEmpty())&&(listeLabel.get(noeudTerminal.getId())).marque()) {
+        	
+        }
         return solution;
     }
-
+    
 }
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -4,7 +4,7 @@ import org.insa.graphs.model.Arc;
 
 import org.insa.graphs.model.Node;
 
-public class Label {
+public class Label implements Comparable<Label>  {
 	private int numNoeud;
 	private Node sommet_courant;
 	private Boolean trouve;
@@ -16,14 +16,28 @@ public class Label {
 	public int getNumNoeud(){
 		return this.numNoeud;
 	}
-	public Label(int numNoeud, Node sommet_courant) {
+	public Label(int numNoeud) {
 		this.numNoeud = numNoeud;
 		this.sommet_courant = sommet_courant;
 		this.trouve = false;
-		this.cost = -1;
+		this.cost = 999999999;
 		this.pere = null;
 	}
 	public void setCost(int cost){
 		this.cost = cost;
 	}
+	public boolean marque() {
+		return this.trouve;
+	}
+	  /**
+     * Compare the ID of this node with the ID of the given node.
+     * 
+     * @param other Node to compare this node with.
+     * 
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(Label other) {
+        return Integer.compare(getCost(), other.getCost());
+    }
 }
